ca97f0a541cb987a65a032c59a681bbd8312d482
Yeray <a21yerayac@iessanclemente.net>
2024-11-21 11:00:44 +0100

    solucion de erroor

--- src/Controller/MainpageController.php
+++ src/Controller/MainpageController.php
@@ -12,22 +12,23 @@ class MainpageController extends AbstractController
     #[Route(path: '/main', name: 'app_homepage')]
     public function index(ReviewRepository $reviewRepository): Response
     {
-        // Verifica si el usuario está autenticado
-        /** @var User $user */
-        $user = $this->getUser();
+        // // Verifica si el usuario está autenticado
+        // /* @var User $user */
+        // $user = $this->getUser();
 
-        // Si está autenticado, obtenemos los usuarios que sigue
-        if ($user) {
-            $seguido = $user->getSiguiendo(); // Devuelve la colección de usuarios que sigues
-            $publicacionesSeguidas = $reviewRepository->findReviewsByUsers($seguido);
-        } else {
-            // Si no está autenticado, obtenemos todas las publicaciones
-            $publicacionesSeguidas = $reviewRepository->findAll();
-        }
+        // // Si está autenticado, obtenemos los usuarios que sigue
+        // if ($user) {
+        //     $seguido = $user->getSiguiendo(); // Devuelve la colección de usuarios que sigues
+        //     $publicacionesSeguidas = $reviewRepository->findReviewsByUsers($seguido);
+        // } else {
+        //     // Si no está autenticado, obtenemos todas las publicaciones
+        //     $publicacionesSeguidas = $reviewRepository->findAll();
+        //
+        // }
 
         // Renderizamos la plantilla con las publicaciones
         return $this->render('main.html.twig', [
-            'publicaciones' => $publicacionesSeguidas,
+            // 'publicaciones' => $publicacionesSeguidas,
         ]);
     }
 }
